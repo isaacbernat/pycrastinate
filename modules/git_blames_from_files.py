928619b58dde6933907bc5539763c23cc7786004
isaacbernat <bernat@wrapp.com>
2014-04-06 23:10:14 +0200

    optimisations on git process_blame_line for files with multiple token matches

--- modules/git_blames_from_files.py
+++ modules/git_blames_from_files.py
@@ -21,28 +21,34 @@ def git_blames_from_files(config, data):
         filename = code_file.split("/")[-1]
         pathname = code_file.split(filename)[0]
         os.chdir(pathname)
-        blame = subprocess.Popen("git blame -e {} -L {},{}".format(
-            filename, count, count), stdout=subprocess.PIPE,
-            shell=True).stdout.read().decode("utf-8")
+        lines_text = "".join(" -L {n},{n}".format(n=nu) for nu in line_numbers)
+        blames = subprocess.Popen("git blame -e {} {}".format(
+            filename, lines_text), stdout=subprocess.PIPE,
+            shell=True).stdout.read().decode("utf-8").split("\n")
         os.chdir(init_dir)
-        email = re.search(regex["email"], blame)
-        email = email.group(1) if email else ""
-        date = re.search(regex["date"], blame).groups()
-        date = datetime.date(int(date[0]), int(date[1]), int(date[2]))
-        code = u"".join(blame.split(")")[1:]).strip()
-        return {
-            "file_path": code_file,
-            "code": code,
-            "date": date,
-            "email": email,
-            "line_count": count,
-            "token": re.search(regex["token"], code).group(), }
+        for blame, line in zip(blames, line_numbers):
+            email = re.search(regex["email"], blame)
+            email = email.group(1) if email else ""
+            date = datetime.date(
+                *[int(d) for d in re.search(regex["date"], blame).groups()])
+            code = u"".join(blame.split(")")[1:]).strip()
+            yield {
+                "file_path": code_file,
+                "code": code,
+                "date": date,
+                "email": email,
+                "line_count": line,
+                "token": re.search(regex["token"], code).group(), }
 
     config = config[__name__.split(".")[-1]]
     regex = prepare_regexes()
     init_dir = os.getcwd()
     for code_file in data:
         with codecs.open(code_file, 'rb', "utf-8") as cf:
+            line_numbers = []
             for count, content in enumerate(cf, start=1):
                 if re.match(regex["blame"], content):
-                    yield process_blame_line()
+                    line_numbers.append(count)
+            if line_numbers:
+                for line in process_blame_line():
+                    yield line
