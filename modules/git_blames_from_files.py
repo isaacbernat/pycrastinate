b7a7511f2f32ddcc82072ea64033ce1fa22ae672
isaacbernat <bernat@wrapp.com>
2014-04-06 19:16:42 +0200

    more efficient git blame processing

--- modules/git_blames_from_files.py
+++ modules/git_blames_from_files.py
@@ -2,6 +2,7 @@ import subprocess
 import datetime
 import re
 import codecs
+import os
 
 
 def git_blames_from_files(config, data):
@@ -16,33 +17,33 @@ def git_blames_from_files(config, data):
             "token": re.compile(u"({})".format(tokens_re), flags),
         }
 
-    def process_blame_line(process_line=True):
-        if process_line:
-            blame = subprocess.Popen("git blame -e {}".format(code_file),
-                                     stdout=subprocess.PIPE, shell=True)
-            blame = blame.stdout.read()
-            if not blame:
-                return
-            blame = blame.decode("utf-8").split("\n")[count]
-            email = re.search(regex["email"], blame)
-            email = email.group(1) if email else ""
-            date = re.search(regex["date"], blame).groups()
-            date = datetime.date(int(date[0]), int(date[1]), int(date[2]))
-            code = u"".join(blame.split(")")[1:]).strip()
-            return {
-                #[2:] because we skip the initial "./"
-                "file_path": code_file[2:],
-                "code": code,
-                "date": date,
-                "email": email,
-                "line_count": count+1,
-                "token": re.search(regex["token"], code).group(), }
+    def process_blame_line():
+        split_code = code_file.split("/")
+        pathname, filename = "/".join(split_code[:-1]), split_code[-1]
+        os.chdir(pathname)
+        blame = subprocess.Popen("git blame -e {} -L {},{}".format(
+            filename, count, count), stdout=subprocess.PIPE,
+            shell=True).stdout.read().decode("utf-8")
+        os.chdir(init_dir)
+        email = re.search(regex["email"], blame)
+        email = email.group(1) if email else ""
+        date = re.search(regex["date"], blame).groups()
+        date = datetime.date(int(date[0]), int(date[1]), int(date[2]))
+        code = u"".join(blame.split(")")[1:]).strip()
+        return {
+            #[2:] because we skip the initial characters "./"
+            "file_path": code_file[2:],
+            "code": code,
+            "date": date,
+            "email": email,
+            "line_count": count,
+            "token": re.search(regex["token"], code).group(), }
 
     config = config[__name__.split(".")[-1]]
     regex = prepare_regexes()
+    init_dir = os.getcwd()
     for code_file in data:
         with codecs.open(code_file, 'rb', "utf-8") as cf:
-            for count, content in enumerate(cf):
-                line = process_blame_line(re.match(regex["blame"], content))
-                if line:
-                    yield line
+            for count, content in enumerate(cf, start=1):
+                if re.match(regex["blame"], content):
+                    yield process_blame_line()
