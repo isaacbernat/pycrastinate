b6a0344e6e0437dcbd3f3f3feb5a6356c08ce8f1
isaacbernat <bernat@wrapp.com>
2014-06-07 21:42:15 +0200

    refactor on _cfg_name to avoid potential name clashes

--- pycrastinate.py
+++ pycrastinate.py
@@ -9,6 +9,7 @@ def run(pipeline, config, enclose):
                   OrderedDict(sorted(pipeline.items())).values(), [])
 
 if __name__ == "__main__":
-    config = "config" if len(sys.argv) == 1 else sys.argv[1].split(".")[0]
-    exec("import {}".format(config))
-    run(config.pipeline, config.data, config.enclose)
+    _cfg_name = "config" if len(sys.argv) == 1 else sys.argv[1].split(".")[0]
+    exec("import {}".format(_cfg_name))
+    _cfg = eval(_cfg_name)
+    run(_cfg.pipeline, _cfg.data, _cfg.enclose)
